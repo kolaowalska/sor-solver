232e005989fcf9bbb1091ebc8328922e4bb1d2f2
KOLA OWALSKA <126875104+kolaowalska@users.noreply.github.com>
2024-12-05 14:34:26 +0100

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -26,3 +26,43 @@ Vector SOR(int n, int m, const Vector& diagonal, const vector<Vector>& bands,
     }
     return x;
 }
+
+int main() {
+    int N, M, L;
+    double omega;
+    cin >> N >> M;
+
+    Vector diagonal(N), rhs(N), x0(N);
+    vector<Vector> bands(M);
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N - M + i; j++) {
+            double t;
+            cin >> t;
+            bands[i].push_back(t);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        cin >> diagonal[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        cin >> rhs[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        cin >> x0[i];
+    }
+
+    cin >> omega >> L;
+
+    Vector x = SOR(N, M, diagonal, bands, rhs, x0, L, omega);
+
+    cout << fixed << setprecision(10);
+    for (double xi : x) {
+        cout << scientific << xi << endl;
+    }
+
+    return 0;
+}
